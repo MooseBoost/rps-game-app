990f1892e80739e601e958da7b6d58d28397710b
Nathaniel Miller <dukeoflaser@gmail.com>
2016-03-27 05:55:14 +0000

    Refactored and fixed win/loss bug. App works and tests pass.

--- app.rb
+++ app.rb
@@ -1,18 +1,9 @@
 require_relative 'config/environment'
 require 'pry'
-#NOTE: When Tests Pass, App does not actually
-# return proper win/loss/tie results.
-# I got the app returning the proper results,
-# but the tests no longer passed.
 
 class GameApp < Sinatra::Base
   get '/rps/:play' do
-    rps_match = RPSGame.new(params[:play].to_sym)
-    @player = rps_match.play
-    @computer = rps_match.computer
-    @winner = rps_match.won
-    @tie = rps_match.tied
-    @loser = rps_match.lost
+    @rps_match = RPSGame.new(params[:play].to_sym)
     erb :rps_game
   end
 end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,6 +1,5 @@
 class RPSGame
-  attr_reader :play, :computer, :won, :lost, :tied
-  
+  attr_reader :play, :computer_play
   @@valid_plays = [:rock, :paper, :scissors]
   
   def initialize(play)
@@ -9,10 +8,7 @@ class RPSGame
         raise PlayTypeError
       else
         @play = play
-        @computer = computer_play
-        @won = won?
-        @lost = lost?
-        @tied = tied?
+        @computer_play = @@valid_plays.sample
       end
     end
   end
@@ -21,27 +17,19 @@ class RPSGame
    @@valid_plays.include? play
   end
   
-  def computer_play
-    @@valid_plays.sample
-  end
-    
   def won?
     winner = {:rock => :scissors, :paper => :rock, :scissors => :paper }
     winner[self.play] == self.computer_play
   end
   
   def tied?
-    tied = {:rock => :rock, :paper => :paper, :scissors => :scissors }
-    tied[self.play] == self.computer_play
+    self.play == self.computer_play
   end
   
   def lost?
-    loser = {:rock => :paper, :paper => :scissors, :scissors => :rock }
-    loser[self.play] == self.computer_play
+    !won? && !tied?
   end
-    
 end
 
 class PlayTypeError < StandardError
-    "Invalid Move"
 end
